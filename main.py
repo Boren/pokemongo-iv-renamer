7761965c9158e8a5c98fc530501d3163d4323689
pxli168 <pxli168@163.com>
2016-08-05 14:07:18 +0800

    Add an iv value filter for renamer
    User can filter the pokemon for rename by iv, default for reaname
    all(iv value is 0)

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ class Renamer(object):
         parser.add_argument("--format", default="%ivsum, %atk/%def/%sta")
         parser.add_argument("-l", "--locale", default="en")
 	parser.add_argument("-d", "--delay", type=int, default=4)
+	parser.add_argument("-iv", "--iv_value", type=int, default=0)
 
         self.config = parser.parse_args()
         self.config.overwrite = True
@@ -162,9 +163,10 @@ class Renamer(object):
             name = name.replace("%name", pokemon_name)
             name = name[:12]
 
-            if pokemon['nickname'] == "NONE" \
-               or pokemon['nickname'] == pokemon_name \
-               or (pokemon['nickname'] != name and self.config.overwrite):
+            if (pokemon['nickname'] == "NONE" \
+                or pokemon['nickname'] == pokemon_name \
+                or (pokemon['nickname'] != name and self.config.overwrite)) \
+                and (iv_percent > self.config.iv_value):
 
                 self.api.nickname_pokemon(pokemon_id=pokemon['id'], nickname=name)
                 response = self.api.call()
